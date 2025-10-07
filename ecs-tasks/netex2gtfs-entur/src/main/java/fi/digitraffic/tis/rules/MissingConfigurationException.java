afd01ab0a6c8d93c3a927f30a0914078fc1ff1ea
Topi Penttinen <212693087+solita-topip@users.noreply.github.com>
2025-10-07 14:12:21 +0300

    Remove unused code

--- ecs-tasks/netex2gtfs-entur/src/main/java/fi/digitraffic/tis/rules/MissingConfigurationException.java
+++ ecs-tasks/netex2gtfs-entur/src/main/java/fi/digitraffic/tis/rules/MissingConfigurationException.java
@@ -4,16 +4,4 @@ public class MissingConfigurationException extends ConfigurationException {
     public MissingConfigurationException(String message) {
         super(message);
     }
-
-    public MissingConfigurationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MissingConfigurationException(Throwable cause) {
-        super(cause);
-    }
-
-    protected MissingConfigurationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
